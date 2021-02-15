2402b2699200154daefa0a802a23a0c53de6f670
EnriqueNestozo <enrique.flores093@gmail.com>
2021-02-15 13:50:01 -0600

    app listelement is created and its models

---
+++ djangovue/listelement/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    clean_url = models.CharField(max_length=255)
+
+class Type(models.Model):
+    title = models.CharField(max_length=255)
+    clean_url = models.CharField(max_length=255)
+
+class Element(models.Model):
+    title = models.CharField(max_length=255)
+    clean_url = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
