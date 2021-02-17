c88cd86d271f1b942257a6b54ac04ab9b074b0d6
EnriqueNestozo <enrique.flores093@gmail.com>
2021-02-17 14:31:03 -0600

    one to many relationship in api

--- djangovue/listelement/serializer.py
+++ djangovue/listelement/serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Element, Category, Type
+from comment.models import Comment
 
-class ElementSerializer(serializers.ModelSerializer):
+
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Element
+        model = Comment
         fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -16,3 +18,14 @@ class TypeSerializer(serializers.ModelSerializer):
         model = Type
         fields = '__all__'
     
+class ElementSerializer(serializers.ModelSerializer):
+    #busca la relacion con el mismo nombre (category, type) e imprimie el __str__
+    # category = serializers.StringRelatedField()
+    # type = serializers.StringRelatedField()
+    type = TypeSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    # comments = serializers.StringRelatedField(many=True)
+    comments = CommentSerializer(read_only=True, many=True)
+    class Meta:
+        model = Element
+        fields = '__all__'
